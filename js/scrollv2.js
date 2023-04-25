7393839f56f93468cd6d5f6abd0b8d1ee1077790
Akshat Garg <akshatg805@gmail.com>
2023-04-25 22:58:40 +0530

    Create scrollv2.js
    Tried another way of achieving sticky nav using js window.scrollY function

---
+++ js/scrollv2.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function () {
+  let element01 = document.querySelector(".logo");
+  let Size01 = 175 - window.scrollY / 3;
+  let element02 = document.querySelector(".brand");
+  let Size02 = window.scrollY / 2.75;
+  let element03 = document.querySelector(".header_div");
+  let opac = 0.5 + window.scrollY / 60;
+  element03.style.backgroundColor = `rgba(205, 205, 240, ${opac})`;
+  let h = 250 - window.scrollY / 2.25;
+  if (window.scrollY <= 225) {
+    element01.style.height = Size01 + "px";
+    element01.style.width = Size01 + "px";
+    element02.style.fontSize = Size02 + "px";
+    element03.style.height = h + "px";
+  }
+});
